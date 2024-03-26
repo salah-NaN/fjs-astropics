011dc74574062f44d6a36feab248c06c7ba7b1ba
Salah Eddin <salah.extras@gmail.com>
2024-03-26 20:01:45 +0100

    commit

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(),
-    react({
-      include: ['**/react/*'],
-    })]
+  integrations: [tailwind(), react({
+    include: ['**/react/*']
+  })],
+  adapter: netlify()
 });
